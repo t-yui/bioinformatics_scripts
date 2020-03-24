86e472b33b16991bb35122b9a0db463eab68e9e1
yuitomo <yuitomo@yuitomonoMacBook-Pro.local>
2020-03-24 19:27:37 +0900

    :+1: add examples

---
+++ gwas_tools/plinkPCA/plot_examples/example.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+python ../plot_pca_gwas.py \
+    -e ./example_data/example.eigenvec \
+    -p ./example_data/example_population.txt \
+    -o ./example_outputs/
